4244ed912cf22b891748cc9120b8921729caa390
wisman101 <lewiskimani18@gmail.com>
2021-03-10 15:54:03 +0300

    okay

--- JS/homePage.js
+++ JS/homePage.js
@@ -1,6 +1,11 @@
 $(document).ready(()=>{
-    $("#dropdown").click(()=>{
+    $(".closebtn").click(()=>{
         document.getElementById("navbar").style.width = "0";
         document.body.style.gridTemplateColumns = "0fr 1fr";
     })
+    $("#dropdown").click(()=>{
+        document.getElementById("navbar").style.width = "20%";
+        document.body.style.gridTemplateColumns = "1fr 4fr";
+    })
+
 })
